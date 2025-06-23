8ce7a27c67a8baee3763804dfd23f17721fa1f70
Dominika Bobik <dominikabobik12@gmail.com>
2025-06-22 20:49:02 -0700

    fix

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,6 +2,6 @@ import styles from './logo.module.css'
 
 export const Logo = (): JSX.Element => {
   return (
-    <div className={styles.text}>Domi's Kitchen</div>
+    <div className={styles.text}>Domi&aposs Kitchen</div>
   )
 }
