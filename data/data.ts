68f3e12346372e0fbca5c8047dc10f5e77ad2c52
Dominika Bobik <dominikabobik12@gmail.com>
2025-06-28 08:02:54 -0700

    add data

--- data/data.ts
+++ data/data.ts
@@ -1,72 +1,77 @@
 export interface RecipeData {
-  title: string,
-  link: string,
-  filters: string[]
+  title: string;
+  link: string;
+  filters: string[];
 }
 export const RecipeList: RecipeData[] = [
   {
-    title: 'Barszcz',
-    link: 'barszcz',
-    filters: ['swieta']
+    title: "Barszcz",
+    link: "barszcz",
+    filters: ["swieta"],
   },
   {
-    title: 'Pierogi',
-    link: 'pierogi',
-    filters: ['swieta']
+    title: "Pierogi",
+    link: "pierogi",
+    filters: ["swieta"],
   },
   {
-    title: 'Sernik',
-    link: 'sernik',
-    filters: ['deser']
+    title: "Sernik",
+    link: "sernik",
+    filters: ["deser"],
   },
   {
-    title: 'Skubaniec',
-    link: 'skubaniec',
-    filters: ['deser']
+    title: "Skubaniec",
+    link: "skubaniec",
+    filters: ["deser"],
   },
   {
-    title: 'Krupnik',
-    link: 'krupnik',
-    filters: ['obiad', 'zupa']
+    title: "Krupnik",
+    link: "krupnik",
+    filters: ["obiad", "zupa"],
   },
   {
-    title: 'Ogórkowa',
-    link: 'ogorkowa',
-    filters: ['obiad', 'zupa']
+    title: "Ogórkowa",
+    link: "ogorkowa",
+    filters: ["obiad", "zupa"],
   },
   {
-    title: 'Pomidorowa',
-    link: 'pomidorowa',
-    filters: ['obiad', 'zupa']
+    title: "Pomidorowa",
+    link: "pomidorowa",
+    filters: ["obiad", "zupa"],
   },
   {
-    title: 'Rosół',
-    link: 'rosol',
-    filters: ['obiad', 'zupa']
+    title: "Rosół",
+    link: "rosol",
+    filters: ["obiad", "zupa"],
   },
   {
-    title: 'Pieczone naleśniki',
-    link: 'pieczone-nalesniki',
-    filters: ['sniadanie']
+    title: "Pieczone naleśniki",
+    link: "pieczone-nalesniki",
+    filters: ["sniadanie"],
   },
   {
-    title: 'Chleb Bananowy',
-    link: 'chleb-bananowy',
-    filters: ['sniadanie', 'deser']
+    title: "Chleb Bananowy",
+    link: "chleb-bananowy",
+    filters: ["sniadanie", "deser"],
   },
   {
-    title: 'Szarlotka',
-    link: 'szarlotka',
-    filters: ['deser']
+    title: "Szarlotka",
+    link: "szarlotka",
+    filters: ["deser"],
   },
   {
-    title: 'Ogórki Małosolne',
-    link: 'ogorki-malosolne',
-    filters: []
+    title: "Ogórki Małosolne",
+    link: "ogorki-malosolne",
+    filters: [],
   },
   {
-    title: 'Budyń',
-    link: 'budyn',
-    filters: ['deser']
-  }
-]
+    title: "Budyń",
+    link: "budyn",
+    filters: ["deser"],
+  },
+  {
+    title: "Chleb",
+    link: "chleb",
+    filters: [""],
+  },
+];
